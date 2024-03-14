2fe087053ff706b1e6b7b3995dedd209294027a7
irvan dzawin nuha <irvandzwinnuha15@gmail.com>
2024-03-14 11:20:04 +0700

    Add project files.

---
+++ jurnal_mod4_kpl/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4
+{
+    internal class kodeBuah
+    {
+        Dictionary<string, string> kode = new Dictionary<string, string>()
+        {
+            {"Apel", "A00"},
+            {"Aprikot", "B00"},
+            {"Alpukat", "C00"},
+            {"Pisang", "D00"),
+            {"Paprika", "E00"},
+            {"Blackberry", "F00"},
+            {"Ceri", "H00"},
+            {"Kelapa", "I00"},
+            {"Jagung", "J00"},
+            {"Kurma", "K00"},
+            {"Durian", "L00"},
+            {"Anggur", "M00"},
+            {"Melon", "N00"},
+            {"Semangka", "O00"};
+        }
+
+    private static void Main(string[] args)
+        {
+        kodeBuah  kodeBuah = new kodeBuah();
+        kodeBuah.name = 
+            Console.WriteLine("kodeBuah.namaBuah);
+
+        }
+    }
+}
---
+++ jurnal_mod4_kpl/posisikaraktergame.cs
@@ -0,0 +1,92 @@
+﻿using jurnal_mod4_kpl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static jurnal_mod4_kpl.posisikaraktergame;
+
+namespace jurnal_mod4_kpl
+{
+    public class posisikaraktergame
+    {
+
+        public enum state
+        {
+           Jongkok,
+           Tengkurap, 
+           Terbang,
+        }
+        public enum Trigger
+        {
+            TombolS,
+            TombolW,
+            TombolX,
+        } 
+    }
+    public class pragram
+    {
+        enum State 
+        {
+            Jongkok,
+            Tengkurap,
+            Terbang,
+            Berdiri
+        }
+
+        private static void Main(String[] arg)
+        {
+            var posisiAwal = posisikaraktergame.state.Jongkok;
+            var posisiKedua = posisikaraktergame.state.Tengkurap;
+            var posisIKetiga = posisikaraktergame.state.Terbang;
+
+            State state = State.Jongkok;
+            string[] screenName =
+                { "Posisi landing",
+                "Posisi  take off"
+                };
+            while (state != State.Terbang)
+            {
+                Console.WriteLine(screenName[(int)state] + " SCREEN");
+                Console.Write("Enter Command : ");
+
+                string command = Console.ReadLine();
+                switch (state)
+                {
+                    case State.Jongkok:
+                        if (command == "tombol s")
+                            state = State.Tengkurap;
+                        else if (command == "tombol w")
+                            state = State.Berdiri;
+                        else
+                            state = State.START;
+                        break;
+                    case State.GAME:
+                        if (command == "ESC")
+                            state = State.PAUSE;
+                        else
+                            state = State.GAME;
+                        break;
+                    case State.PAUSE:
+                        if (command == "BACK")
+                            state = State.GAME;
+                        else if (command == "HOME")
+                            state = State.START;
+                        else if (command == "QUIT")
+                            state = State.EXIT;
+                        else
+                            state = State.PAUSE;
+                        break;
+                }
+            }
+            Console.WriteLine("EXIT SCREEN");
+        }
+        public void activeTrigger(Trigger trigger) 
+        {
+            
+
+            
+        }
+    }
+
+}
